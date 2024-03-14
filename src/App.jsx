7539943fadcaf0f43cdc42877e7f02708ec926f2
Sergio <norgestream@gmail.com>
2024-03-14 23:00:32 +0200

    add ability to fetch places that was choosen earlier

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,15 @@ function App() {
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
   useEffect(() => {
+    const storedIds = JSON.parse(localStorage.getItem('selected places') || []);
+    const storedPlaces = storedIds.map((id) => 
+      AVAILABLE_PLACES.find((place) => place.id === id)
+    );
+
+    setPickedPlaces(storedPlaces);
+  }, []);
+
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const sortedPlaces = sortPlacesByDistance(
         AVAILABLE_PLACES, 
@@ -55,6 +64,11 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selected places') || []);
+    localStorage.setItem(
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current ))
+    );
   }
 
   return (
